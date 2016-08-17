70e68698455d156430b911376a27dce8799b0113
memresener <m.emre.sener@gmail.com>
2016-08-17 10:07:48 +0100

    Add files via upload
    using external adc.

--- firmware.ino
+++ firmware.ino
@@ -17,19 +17,17 @@
 
 //Communication parameters
 #define BAUDRATE 115200   //Serial interfaces communication speed (bps)
-#define SAMPLER_PIN_A A0  //Input A
-#define SAMPLER_PIN_B A1  //Input B 
+
 
 /* Setup */
-
+Adafruit_ADS1015 adc;
 RTx* phone=new RTx();
 DACController* ctrl=new DACController(STEPSIZE, LINE_LENGTH, _SDI, _SCK, LOAD, LDAC, RNG);
-SignalSampler* sampler=new SignalSampler(SAMPLER_PIN_A, SAMPLER_PIN_B, SAMPLE_SIZE);
+SignalSampler* sampler=new SignalSampler(adc, SAMPLE_SIZE);
 Scanner* scanner=new Scanner(*ctrl, *sampler, *phone, LINE_LENGTH);
 
 //This function runs once, when the arduino starts
 void setup() {
-   
   Serial.begin(BAUDRATE);
 }
 
@@ -56,7 +54,7 @@ void loop() {
       int CUSTOM_SAMPLE_SIZE=Serial.parseInt();
      
       ctrl =new DACController(CUSTOM_STEPSIZE, CUSTOM_LINE_LENGTH, _SDI, _SCK, LOAD, LDAC, RNG);
-      sampler = new SignalSampler(SAMPLER_PIN_A, SAMPLER_PIN_B, CUSTOM_SAMPLE_SIZE);
+      sampler = new SignalSampler(adc, CUSTOM_SAMPLE_SIZE);
       scanner = new Scanner(*ctrl, *sampler, *phone, CUSTOM_LINE_LENGTH);     
     
   }
